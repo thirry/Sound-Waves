aa2dfa6b41ddcebae2cdadc7403bb7a8213a146a
Thrirry <tam300497@gmail.com>
2018-01-24 23:30:25 +0700

    Initial Commit

---
+++ Fubiz/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Fubiz
+//
+//  Created by Gone on 1/24/18.
+//  Copyright © 2018 Gone. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
